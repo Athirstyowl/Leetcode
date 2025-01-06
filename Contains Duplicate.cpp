aa12341d8c45469a291cdec242b7d99b6e1415a9
Atharva Gholap <92541899+Athirstyowl@users.noreply.github.com>
2025-01-06 20:26:11 +0530

    Create Contains Duplicate.cpp

---
+++ Contains Duplicate.cpp
@@ -0,0 +1,40 @@
+// class Solution {
+// public:
+//     bool hasDuplicate(vector<int>& nums) {
+//         bool noDuplicate = true;
+//         int currentIndex = 0;
+//         while(noDuplicate && currentIndex < nums.size()){
+//             for(int i = 0; i < nums.size(); i++){
+//                 if(nums[currentIndex] == nums[i] && currentIndex != i){
+//                     noDuplicate = false;
+//                     return true;
+//                 }
+//             }
+//             currentIndex++;
+//         }
+//         return false;
+//     }
+// };
+
+
+class Solution {
+    public:
+    bool hasDuplicate(vector<int>& nums) {
+        int length = nums.size();
+
+        map<int,int> numberMap;
+        for(int i = 0; i < length; i++){
+            int n = nums[i];
+            if(numberMap[n]){
+                numberMap[n]++;
+                if(numberMap[n] > 1){
+                    return true;
+                }
+            }
+            else{
+                numberMap[n] = 1;
+            }
+        }
+        return false;
+    }
+};
